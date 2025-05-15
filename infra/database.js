8afc89a4c0b56a928a463c4a3dfb51dbeb6d8cba
mariliavieira <65554465+mariliavieira@users.noreply.github.com>
2025-05-15 13:21:19 +0000

    `database.js`and `.env`files

--- infra/database.js
+++ infra/database.js
@@ -2,11 +2,11 @@ import { Client } from "pg";
 
 async function query(queryObject) {
   const client = new Client({
-    host: "localhost",
-    port: 5432,
-    user: "postgres",
-    database: "postgres",
-    password: "local_password",
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
   });
   await client.connect(); //Esse é um comando assíncrono, por isso o uso do await, comando assíncrono significa que roda em segundo plano permitindo que o restante do código continue sendo executado
   const result = await client.query(queryObject);
